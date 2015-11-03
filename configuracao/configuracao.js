ed1c37ffb1188cadd5ab88123c84aba2f2b3452f
umd <leofelippedias@facebook.com>
2015-11-03 18:03:56 -0200

    Edt. variaveis

--- configuracao/configuracao.js
+++ configuracao/configuracao.js
@@ -2,8 +2,8 @@ module.exports = {
   port: 5222,
   domain: 'localhost',
 
-  // Registrar
-  logger: false,
+  // Registrar eventos e atividades (erros, infos) principalmente dos clientes conectados
+  logger: true,
 
   //tls: {
   //  keyPath: '/etc/xmpp-server/tls/localhost-key.pem',
