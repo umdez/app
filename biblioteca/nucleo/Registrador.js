238057208f7d6a34fc61a598df61784e2d8b8d31
umd <leofelippedias@facebook.com>
2015-11-03 18:28:22 -0200

    Adc. um registrador que utiliza bunyan para ser utilizado pelos outros arquivos.

--- biblioteca/nucleo/Registrador.js
+++ biblioteca/nucleo/Registrador.js
@@ -1,88 +1,38 @@
 'use strict';
 
-var registrador = require('winston');
+var bunyan = require('bunyan');
 
-/*
- * @param este é definido na configuração se for necessario ligar o registro para o servidor determinado 
- */
-function Registrador() {
-  
-}
-
-Registrador.prototype.configurar = function(servidor, ligar) {
-  
-}
-
-/* Se for ligado vai fazer o registro dos eventos para um dado servidor.
- *
- * @param o determinado servidor em que iremos registrar os eventos
- */
-Registrador.prototype.adcRegistroEventosPara = function (servidor, seLigar) {
-  
-  // Aqui faremos o registro para cada um dos eventos 
-  // Aqui conseguiremos escutar os eventos em todos objetos ligados aquele servidor, incluindo as rotas, 
-  // gerenciamento de sessão, rotas S2S, as conexões, etc.
-  if(seLigar) {
-	  
-	// http://xmpp.org/extensions/xep-0160.html
-    var formatarRegistro = function(cliente, mensagem) {
-      return [cliente.socket.remoteAddress, cliente.streamId, mensagem].join(" "); 
-    }  
-	  
-    servidor.on("connect", function(cliente) {
-      registrador.debug(formatarRegistro(cliente, "connected"));
-
-      cliente.on('session-started', function() {
-        registrador.info(formatarRegistro(cliente, stanza.toString()));
-      });
-
-      cliente.on('auth-success', function(jid) {
-        registrador.info(formatarRegistro(cliente, "auth-success " + jid));
-      });
-
-      cliente.on('online', function() {
-        registrador.info(formatarRegistro(cliente, "online " + cliente.jid));
-      });
-
-      cliente.on('auth-failure', function(jid) {
-        registrador.info(formatarRegistro(cliente, "auth-failure " + jid));
-      });
-
-      cliente.on('registration-success', function(jid) {
-        registrador.info(formatarRegistro(cliente, "registration-success " + jid));
-      });
-
-      cliente.on('registration-failure', function(jid) {
-        registrador.info(formatarRegistro(cliente, "registration-failure " + jid));
-      });
-    });
-
-    servidor.on("s2sReady", function(s2s) {
-      // console.log("S2S ready");
-      // s2s.on("newStream", function(stream) {
-      // console.log("New Stream");
-      // });
-    });
-
-    servidor.on("c2sRoutersReady", function(router) {
-      // console.log("Router ready")
-    })
-  }
-
+// Usa as variaveis de ambiente LOGSTASH_HOST e LOGSTASH_PORT
+// para enviar os registros para logstash por meio de udp.
+var configuracao = {
+  name: 'servidor-xmpp',
+  streams: [{
+    level: 'debug',
+    stream: process.stdout
+  }],
+  level: 'debug'
 };
 
-Registrador.prototype.debug = function(mensagem) {
-  // Obsoleto, deletar!
-  if (this.seLigado) {
-    registrador.debug(mensagem);  
-  }
-} 
+if (process.env.LOGSTASH_HOST && process.env.LOGSTASH_PORT) {
+  console.log('logstash habilitado');
+  configuracao.streams.push({
+    level: 'debug',
+    type: 'raw',
+    stream: require('bunyan-logstash-tcp').createStream({
+      host: process.env.LOGSTASH_HOST,
+      port: process.env.LOGSTASH_PORT
+    })
+  });
+} else {
+  console.log('logstash não habilitado');
+}
 
-Registrador.prototype.info = function(mensagem) {
-  // Obsoleto, deletar!
-  if (this.seLigado) {
-    registrador.info(mensagem);  
-  }
-} 
+var registro = bunyan.createLogger(configuracao);
 
-module.exports = Registrador;
+function registrador(nome) {
+  return registro.child({
+    widget_type: nome // jshint ignore:line
+  });
+}
+
+module.exports = registrador;
